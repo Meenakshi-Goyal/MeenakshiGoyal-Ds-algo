f07b6836e1f80a64eac1b45b32b463ac5abae370
Meenakshi-Goyal <57310401+Meenakshi-Goyal@users.noreply.github.com>
2020-06-02 11:37:13 +0530

    Update Coprime.java

--- Day 1/Coprime.java
+++ Day 1/Coprime.java
@@ -1,1 +1,35 @@
-
+import java.util.*;
+public class Solution2 {
+ 
+static int gcd(int a, int b)
+{
+// Everything divides 0
+if (a == 0)
+return b;
+if (b == 0)
+return a;
+ 
+// base case
+if (a == b)
+return a;
+ 
+// a is greater
+if (a > b)
+return gcd(a-b, b);
+return gcd(a, b-a);
+}
+ 
+// Driver method
+public static void main(String[] args)
+{
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+int maxCoPrime=1;
+for(int i=1;i<n-1;i++){
+if(gcd(i,n)==1){
+maxCoPrime=i;
+}
+}
+System.out.println(maxCoPrime);
+}
+}
