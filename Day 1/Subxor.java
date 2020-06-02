e5664fa20038fa8a9b0fa0e96c699ba9e73e8d35
Meenakshi-Goyal <57310401+Meenakshi-Goyal@users.noreply.github.com>
2020-06-02 11:28:41 +0530

    Update Subxor.java

--- Day 1/Subxor.java
+++ Day 1/Subxor.java
@@ -1,1 +1,23 @@
-
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+       int a[] = new int[n];
+       for(int i=0 ;i<n;i++){
+           a[i] = sc.nextInt();
+       }
+         
+    int res = 0; 
+   for(int i = 0;i<n;i++){
+       res = res^a[i];
+ 
+   }if(res != 0){
+       System.out.println("1");
+   }else{
+       System.out.println("-1");
+   }
+    }
+}
+ 
