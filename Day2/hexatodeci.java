123958e4db724d98e9669ee108bbd754cca04c57
Meenakshi-Goyal <57310401+Meenakshi-Goyal@users.noreply.github.com>
2020-06-02 19:42:07 +0530

    Create hexatodeci.java

---
+++ Day2/hexatodeci.java
@@ -0,0 +1,12 @@
+public static int hextodeci(String hex){  
+    String digits = "0123456789ABCDEF";  
+             hex = hex.toUpperCase();  
+             int val = 0;  
+             for (int i = 0; i < hex.length(); i++)  
+             {  
+                 char c = hex.charAt(i);  
+                 int d = digits.indexOf(c);  
+                 val = 16*val + d;  
+             }  
+             return val;  
+} 
