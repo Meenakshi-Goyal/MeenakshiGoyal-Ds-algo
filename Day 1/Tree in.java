9af962ece9f56a06ebda28cfe486cecd21681c4d
Meenakshi-Goyal <57310401+Meenakshi-Goyal@users.noreply.github.com>
2020-06-02 11:48:25 +0530

    Create Tree in.java

---
+++ Day 1/Tree in.java
@@ -0,0 +1,12 @@
+ public static void inOrder(Node root) {
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+
+        System.out.print(root.data + " ");
+        
+        inOrder(root.right);
+
+    }
+
